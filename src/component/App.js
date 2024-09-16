0ae5d9ee0cda69bd5af00791cb780aa0f227c791
Bohdan Martines <bohdan.martines@gmail.com>
2024-09-16 16:34:40 +0200

    Create AppContent and WelcomeContent classes

--- src/component/App.js
+++ src/component/App.js
@@ -2,11 +2,19 @@ import './App.css';
 import logo from '../logo.jpg';
 
 import Header from './Header';
+import AppContent from "./AppContent";
 
 function App() {
     return (
         <div>
             <Header pageTitle="City Manager" logoSrc={logo}>Placeholder</Header>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col">
+                        <AppContent/>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
