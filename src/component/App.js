60a0a58468bf9a31245a50c0c1bda0c31e97d87e
Bohdan Martines <bohdan.martines@gmail.com>
2024-09-16 13:26:50 +0200

    Move components into a new folder

---
+++ src/component/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import logo from '../logo.svg';
+
+function App() {
+    return (
+        <div>
+            <h1>Placeholder</h1>
+        </div>
+    );
+}
+
+export default App;
