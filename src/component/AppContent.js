991f310f0e364123914741fbd5e9ceffaa0559af
Bohdan Martines <bohdan.martines@gmail.com>
2024-09-19 14:25:51 +0200

    Introduce the error page, decommission the welcome page

--- src/component/AppContent.js
+++ src/component/AppContent.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import WelcomeContent from "./WelcomeContent";
+import ErrorComponent from "./ErrorComponent";
 import ProtectedContent from "./ProtectedContent";
 import AuthComponent from "./AuthComponent";
 import {request} from "../backend_client";
@@ -25,7 +25,7 @@ export default class AppContent extends React.Component {
             this.state.setActiveComponent("protected");
             setAuthTokens(response.data.accessToken, response.data.refreshToken);
         }).catch((error) => {
-            this.state.setActiveComponent("welcome");
+            this.state.setActiveComponent("error");
         });
     }
 
@@ -44,14 +44,14 @@ export default class AppContent extends React.Component {
             this.state.setActiveComponent("protected");
             setAuthTokens(response.data.accessToken, response.data.refreshToken);
         }).catch((error) => {
-            this.state.setActiveComponent("welcome");
+            this.state.setActiveComponent("error");
         });
     }
 
     render() {
         return (
             <div>
-                {this.state.getActiveComponent() === "welcome" && <WelcomeContent/>}
+                {this.state.getActiveComponent() === "error" && <ErrorComponent/>}
                 {this.state.getActiveComponent() === "protected" && <ProtectedContent/>}
                 {this.state.getActiveComponent() === "login" &&
                     <AuthComponent onLogin={this.onLogin} onRegister={this.onRegister}/>}
--- src/component/ErrorComponent.js
+++ src/component/ErrorComponent.js
@@ -1,13 +1,13 @@
 import * as React from "react";
 
-export default class WelcomeContent extends React.Component {
+export default class ErrorComponent extends React.Component {
     render() {
         return (
             <div className="row justify-content-md-center">
                 <div className="jumbotron jumbotron-fluid">
                     <div className="container">
-                        <h1 className="display-4">Welcome to City Manager application</h1>
-                        <p className="lead">Would you like to log in?</p>
+                        <h1 className="display-4">Error page</h1>
+                        <p className="lead">Looks like something went wrong, please try again...</p>
                     </div>
                 </div>
             </div>
