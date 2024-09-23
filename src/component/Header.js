da5abf3a75a80aed8a5b0dfe769d726d94224857
Bohdan Martines <bohdan.martines@gmail.com>
2024-09-23 14:05:02 +0200

    Migrate Header to function components

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,39 +1,27 @@
 import * as React from 'react';
 import {clearAuthTokens} from "./session_state_helper";
 
-export default class Header extends React.Component {
+export default function Header({logoSrc, pageTitle, getActiveComponent, setActiveComponent}) {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            logoSrc: props.logoSrc,
-            pageTitle: props.pageTitle,
-            getActiveComponent: props.getActiveComponent,
-            setActiveComponent: props.setActiveComponent
-        }
-    }
-
-    logout = () => {
-        this.state.setActiveComponent("login");
+    const logout = () => {
+        setActiveComponent("login");
         clearAuthTokens();
     }
 
-    render() {
-        return (
-            <header className="App-header">
-                <div className="row">
-                    <div className="col-4"></div>
-                    <div className="col-4 text-center">
-                        <img src={this.state.logoSrc} alt="logo" className="App-logo"/>
-                        <h1 className="App-title">{this.state.pageTitle}</h1>
-                    </div>
-                    <div className="col-3"></div>
-                    <div className="col-1 d-flex align-items-center">
-                        {this.state.getActiveComponent() === "protected" &&
-                            <button className="btn btn-outline-info" onClick={this.logout}>Logout</button>}
-                    </div>
+    return (
+        <header className="App-header">
+            <div className="row">
+                <div className="col-4"></div>
+                <div className="col-4 text-center">
+                    <img src={logoSrc} alt="logo" className="App-logo"/>
+                    <h1 className="App-title">{pageTitle}</h1>
                 </div>
-            </header>
-        );
-    }
+                <div className="col-3"></div>
+                <div className="col-1 d-flex align-items-center">
+                    {getActiveComponent() === "protected" &&
+                        <button className="btn btn-outline-info" onClick={logout}>Logout</button>}
+                </div>
+            </div>
+        </header>
+    );
 }
