9112a833551ef4645430c120f2d2110a1bf41d2a
Bohdan Martines <bohdan.martines@gmail.com>
2024-09-23 14:26:53 +0200

    Migrate ErrorComponent to function components

--- src/component/ErrorComponent.js
+++ src/component/ErrorComponent.js
@@ -1,16 +1,14 @@
 import * as React from "react";
 
-export default class ErrorComponent extends React.Component {
-    render() {
-        return (
-            <div className="row justify-content-md-center">
-                <div className="jumbotron jumbotron-fluid">
-                    <div className="container">
-                        <h1 className="display-4">Error page</h1>
-                        <p className="lead">Looks like something went wrong, please try again...</p>
-                    </div>
+export default function ErrorComponent() {
+    return (
+        <div className="row justify-content-md-center">
+            <div className="jumbotron jumbotron-fluid">
+                <div className="container">
+                    <h1 className="display-4">Error page</h1>
+                    <p className="lead">Looks like something went wrong, please try again...</p>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
