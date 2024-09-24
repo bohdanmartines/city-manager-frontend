f1a51030e621dbe67593353c052f93dbf1cd04d8
Bohdan Martines <bohdan.martines@gmail.com>
2024-09-24 18:50:44 +0200

    Clean up. Collapse empty tags

--- src/component/AppContent.js
+++ src/component/AppContent.js
@@ -9,11 +9,11 @@ export default function AppContent({setLoggedIn}) {
     return (
         <div>
             <Routes>
-                <Route path={ROOT} element={<Navigate to={HOME}/>}></Route>
-                <Route path={HOME} element={<ProtectedContent/>}></Route>
-                <Route path={LOGIN} element={<AuthComponent setLoggedIn={setLoggedIn}/>}></Route>
-                <Route path={ERROR} element={<ErrorComponent/>}></Route>
-                <Route path="*" element={<ErrorComponent message="Page not found"/>}></Route>
+                <Route path={ROOT} element={<Navigate to={HOME}/>}/>
+                <Route path={HOME} element={<ProtectedContent/>}/>
+                <Route path={LOGIN} element={<AuthComponent setLoggedIn={setLoggedIn}/>}/>
+                <Route path={ERROR} element={<ErrorComponent/>}/>
+                <Route path="*" element={<ErrorComponent message="Page not found"/>}/>
             </Routes>
         </div>
     );
