00c138970acd808665a5be80f4e52d654312f1e7
Bohdan Martines <bohdan.martines@gmail.com>
2024-09-25 19:00:37 +0200

    Basic ticket details view

--- src/component/TicketDetailsComponent.js
+++ src/component/TicketDetailsComponent.js
@@ -1,16 +1,28 @@
 import {useNavigate, useParams} from "react-router-dom";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {isAuthenticated} from "../helper/session_state_helper";
-import {LOGIN} from "../helper/path";
+import {ERROR, LOGIN} from "../helper/path";
+import {request} from "../helper/backend_client";
 
 export default function TicketDetails() {
 
     const {ticketId} = useParams();
+    const [ticket, setTicket] = useState({});
     const navigate = useNavigate();
 
     useEffect(() => {
         if (!isAuthenticated()) {
             navigate(LOGIN)
+        } else {
+            request(
+                "ticket/" + ticketId,
+                "GET",
+                {}
+            ).then(response => {
+                setTicket(response.data);
+            }).catch((error) => {
+                navigate(ERROR);
+            });
         }
     }, [navigate])
 
@@ -21,7 +33,11 @@ export default function TicketDetails() {
     return (
         <div>
             <h2>Ticket Details</h2>
-            <p>Ticket ID: {ticketId}</p>
+            <p><strong>ID:</strong> {ticket.id}</p>
+            <p><strong>Title:</strong> {ticket.title}</p>
+            <p><strong>Description:</strong> {ticket.description}</p>
+            <p><strong>Status:</strong> {ticket.status}</p>
+            <p><strong>Created At:</strong> {new Date(ticket.createdAt).toLocaleString()}</p>
         </div>
     );
 };
