7ac1d7c2bb2d543ff9e9ff35d6820bad3ac2ae2a
Bohdan Martines <bohdan.martines@gmail.com>
2024-09-25 19:39:58 +0200

    Working version of ticket details page

--- src/component/TicketDetailsComponent.js
+++ src/component/TicketDetailsComponent.js
@@ -11,6 +11,7 @@ export default function TicketDetails() {
     const navigate = useNavigate();
 
     useEffect(() => {
+        console.log("Showing details for ticket ID: " + ticketId)
         if (!isAuthenticated()) {
             navigate(LOGIN)
         } else {
@@ -30,14 +31,53 @@ export default function TicketDetails() {
         return <div>No ticket selected</div>;
     }
 
+    function formatDateTime(date) {
+        if (date === undefined) {
+            return date;
+        }
+        if (date.indexOf('.') !== -1) {
+            date = date.split('.')[0];
+        }
+        if (date.indexOf('T') !== -1) {
+            const splitDate = date.split('T');
+            return splitDate[0] + " " + splitDate[1];
+        }
+        return date;
+    }
+
     return (
         <div>
-            <h2>Ticket Details</h2>
-            <p><strong>ID:</strong> {ticket.id}</p>
-            <p><strong>Title:</strong> {ticket.title}</p>
-            <p><strong>Description:</strong> {ticket.description}</p>
-            <p><strong>Status:</strong> {ticket.status}</p>
-            <p><strong>Created At:</strong> {new Date(ticket.createdAt).toLocaleString()}</p>
+            <div className="row justify-content-md-center">
+                <div className="col-6">
+                    <div className="container mt-4">
+                        <h3 className="mb-3">Ticket Details</h3>
+                        <table className="table table-striped table-hover">
+                            <tbody>
+                            <tr key="id">
+                                <th scope="row">Ticket ID</th>
+                                <td>{ticket.id}</td>
+                            </tr>
+                            <tr key="title">
+                                <th scope="row">Title</th>
+                                <td>{ticket.title}</td>
+                            </tr>
+                            <tr key="description">
+                                <th scope="row">Description</th>
+                                <td>{ticket.description}</td>
+                            </tr>
+                            <tr key="status">
+                                <th scope="row">Status</th>
+                                <td>{ticket.status}</td>
+                            </tr>
+                            <tr key="createdAt">
+                                <th scope="row">Creation time</th>
+                                <td>{formatDateTime(ticket.createdAt)}</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
