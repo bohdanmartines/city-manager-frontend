6f412b7af2284c19d76d6b518706546fbb0c3529
Bohdan Martines <bohdan.martines@gmail.com>
2024-09-25 15:30:01 +0200

    Call backend when the ticket creation form has been submitted

--- src/component/NewTicketComponent.js
+++ src/component/NewTicketComponent.js
@@ -1,15 +1,34 @@
-import { useState } from 'react';
+import {useEffect, useState} from 'react';
+import {useNavigate} from "react-router-dom";
 import {Button, Form} from "react-bootstrap";
+import {isAuthenticated} from "../helper/session_state_helper";
+import {ERROR, LOGIN} from "../helper/path";
+import {request} from "../helper/backend_client";
 
 export default function NewTicketComponent() {
 
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
     const [validated, setValidated] = useState(false);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!isAuthenticated()) {
+            navigate(LOGIN)
+        }
+    }, [navigate])
 
     const onCreate = (newTicket) => {
-        console.log("About to create a new ticket", newTicket);
-        // TODO implement a call to the backend to create a ticket
+        request(
+            "ticket/new",
+            "POST",
+            newTicket
+        ).then(response => {
+            console.log("New ticket created!");
+            // TODO Redirect to the ticket page
+        }).catch(() => {
+            navigate(ERROR);
+        });
     };
 
     const handleSubmit = (e) => {
@@ -32,7 +51,7 @@ export default function NewTicketComponent() {
     return (
         <div className="row justify-content-md-center">
             <div className="col-6">
-                <div className="container mt-5">
+                <div className="container mt-4">
                     <h2 className="mb-4">Create New Ticket</h2>
                     <Form noValidate validated={validated} onSubmit={handleSubmit}>
                         <Form.Group className="mb-3" controlId="formTitle">
