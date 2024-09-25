9cedac6a0a4e8427f9ca1534f03bf3caf7f99a07
Bohdan Martines <bohdan.martines@gmail.com>
2024-09-25 10:02:00 +0200

    Rename ProtectedContent to Dashboard. Make it show a table with tickets data from the backend

--- src/component/AppContent.js
+++ src/component/AppContent.js
@@ -1,5 +1,5 @@
 import ErrorComponent from "./ErrorComponent";
-import ProtectedContent from "./ProtectedContent";
+import DashboardComponent from "./DashboardComponent";
 import AuthComponent from "./AuthComponent";
 import {Navigate, Route, Routes} from "react-router-dom";
 import {ERROR, HOME, LOGIN, ROOT} from "../helper/path";
@@ -10,7 +10,7 @@ export default function AppContent({setLoggedIn}) {
         <div>
             <Routes>
                 <Route path={ROOT} element={<Navigate to={HOME}/>}/>
-                <Route path={HOME} element={<ProtectedContent/>}/>
+                <Route path={HOME} element={<DashboardComponent/>}/>
                 <Route path={LOGIN} element={<AuthComponent setLoggedIn={setLoggedIn}/>}/>
                 <Route path={ERROR} element={<ErrorComponent/>}/>
                 <Route path="*" element={<ErrorComponent message="Page not found"/>}/>
---
+++ src/component/DashboardComponent.js
@@ -0,0 +1,59 @@
+import * as React from "react";
+import {useEffect, useState} from "react";
+import {request} from "../helper/backend_client";
+import {isAuthenticated} from "../helper/session_state_helper";
+import {useNavigate} from "react-router-dom";
+import {LOGIN} from "../helper/path";
+
+export default function DashboardComponent() {
+
+    const [tickets, setTickets] = useState([]);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!isAuthenticated()) {
+            navigate(LOGIN)
+        } else {
+            request(
+                "ticket",
+                "GET",
+                {}
+            ).then(response => {
+                setTickets(response.data);
+            });
+        }
+    }, [navigate])
+
+    return (
+        <div className="row justify-content-md-center">
+            <div className="col-6">
+                <div className="container mt-4">
+                    <h3 className="mb-3">Dashboard</h3>
+                    <p className="lead">View existing tickets or create a new one</p>
+                    <table className="table table-striped">
+                        <thead>
+                        <tr>
+                            <th scope="col">Ticket ID</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Description</th>
+                            <th scope="col">Status</th>
+                            <th scope="col">Created At</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {tickets.map((ticket) => (
+                            <tr key={ticket.id}>
+                                <th scope="row">{ticket.id}</th>
+                                <td>{ticket.title}</td>
+                                <td>{ticket.description}</td>
+                                <td>{ticket.status}</td>
+                                <td>{new Date(ticket.createdAt).toLocaleDateString()}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    );
+}
