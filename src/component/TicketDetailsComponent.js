6b5388225c0bf4e5cadcab67c57ee00100196d53
Bohdan Martines <bohdan.martines@gmail.com>
2024-09-25 16:09:43 +0200

    Redirect to the login page if an anonymous user is trying to access ticket details

--- src/component/TicketDetailsComponent.js
+++ src/component/TicketDetailsComponent.js
@@ -1,8 +1,18 @@
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
+import {useEffect} from "react";
+import {isAuthenticated} from "../helper/session_state_helper";
+import {LOGIN} from "../helper/path";
 
 export default function TicketDetails() {
 
     const {ticketId} = useParams();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!isAuthenticated()) {
+            navigate(LOGIN)
+        }
+    }, [navigate])
 
     if (!ticketId) {
         return <div>No ticket selected</div>;
