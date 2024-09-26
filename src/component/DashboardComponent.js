76559860c4d612763206e877e35e42430d1e29fb
Bohdan Martines <bohdan.martines@gmail.com>
2024-09-26 13:16:10 +0200

    Rewrite pagination using React bootstrap

--- src/component/DashboardComponent.js
+++ src/component/DashboardComponent.js
@@ -5,12 +5,13 @@ import {isAuthenticated} from "../helper/session_state_helper";
 import {Link, useNavigate} from "react-router-dom";
 import {ERROR, LOGIN, NEW_TICKET, TICKET_DETAILS_BASE_PATH} from "../helper/path";
 import {toLocaleDateString} from "../helper/date_utils";
+import {Pagination} from "react-bootstrap";
 
 export default function DashboardComponent() {
 
     const [tickets, setTickets] = useState([]);
     const [page, setPage] = useState(0);
-    const [size, setSize] = useState(10);
+    const size = 10;
     const [totalPages, setTotalPages] = useState(0);
     const navigate = useNavigate();
 
@@ -37,10 +38,21 @@ export default function DashboardComponent() {
                 <div className="container mt-4">
                     <h3 className="mb-3">Dashboard</h3>
                     <p className="lead">View existing tickets or create a new one <Link to={NEW_TICKET}>here</Link>.</p>
-                    <div>
-                        <button onClick={() => setPage(page - 1)} disabled={page === 0}>Previous</button>
-                        <button onClick={() => setPage(page + 1)} disabled={page + 1 === totalPages}>Next</button>
-                    </div>
+                    <Pagination>
+                        <Pagination.First onClick={() => setPage(0)} disabled={page === 0} />
+                        <Pagination.Prev onClick={() => setPage(page - 1)} disabled={page === 0} />
+                        {[...Array(totalPages)].map((_, index) => (
+                            <Pagination.Item
+                                key={index}
+                                active={index === page}
+                                onClick={() => setPage(index)}
+                            >
+                                {index + 1}
+                            </Pagination.Item>
+                        ))}
+                        <Pagination.Next onClick={() => setPage(page + 1)} disabled={page === totalPages - 1} />
+                        <Pagination.Last onClick={() => setPage(totalPages - 1)} disabled={page === totalPages - 1} />
+                    </Pagination>
                     <table className="table table-striped table-hover">
                         <thead>
                         <tr>
