e65bc6376b988ab159430642917796b6ff6f41ad
Bohdan Martines <bohdan.martines@gmail.com>
2024-09-27 12:49:59 +0200

    Add an option to select the number of tickets per page

--- src/component/DashboardComponent.js
+++ src/component/DashboardComponent.js
@@ -37,24 +37,10 @@ export default function DashboardComponent() {
         <div className="row justify-content-md-center">
             <div className="col-6">
                 <div className="container mt-4">
-                    <h3 className="mb-3">Dashboard</h3>
+                    <h3 className="mb-2">Dashboard</h3>
                     <p className="lead">View existing tickets or create a new one <Link to={NEW_TICKET}>here</Link>.</p>
-                    <div className="mb-3">
-                        <label htmlFor="ticketsPerPage" className="form-label">Tickets per page:</label>
-                        <select
-                            id="ticketsPerPage"
-                            className="form-select"
-                            value={size}
-                            onChange={(e) => setSize(Number(e.target.value))}
-                        >
-                            <option value={5}>5</option>
-                            <option value={10}>10</option>
-                            <option value={25}>25</option>
-                            <option value={50}>50</option>
-                            <option value={100}>100</option>
-                            <option value={tickets.length}>All</option>
-                        </select>
-                        <Pagination>
+                    <div className="d-flex justify-content-between align-items-center mt-4 mb-3">
+                        <Pagination className="m-0">
                             <Pagination.First onClick={() => setPage(0)} disabled={page === 0}/>
                             <Pagination.Prev onClick={() => setPage(page - 1)} disabled={page === 0}/>
                             {[...Array(totalPages)].map((_, index) => (
@@ -67,8 +53,25 @@ export default function DashboardComponent() {
                                 </Pagination.Item>
                             ))}
                             <Pagination.Next onClick={() => setPage(page + 1)} disabled={page === totalPages - 1}/>
-                            <Pagination.Last onClick={() => setPage(totalPages - 1)} disabled={page === totalPages - 1}/>
+                            <Pagination.Last onClick={() => setPage(totalPages - 1)}
+                                             disabled={page === totalPages - 1}/>
                         </Pagination>
+                        <div className="d-flex justify-content-end align-items-center">
+                            <label htmlFor="ticketsPerPage" className="text-nowrap me-3">Tickets per page:</label>
+                            <select
+                                id="ticketsPerPage"
+                                className="form-select"
+                                value={size}
+                                onChange={(e) => setSize(Number(e.target.value))}
+                            >
+                                <option value={5}>5</option>
+                                <option value={10}>10</option>
+                                <option value={25}>25</option>
+                                <option value={50}>50</option>
+                                <option value={100}>100</option>
+                                <option value={tickets.length}>All</option>
+                            </select>
+                        </div>
                     </div>
                     <table className="table table-striped table-hover">
                         <thead>
