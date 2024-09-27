ae8a2da33bffe2417f75961d5f77323020c59bb8
Bohdan Martines <bohdan.martines@gmail.com>
2024-09-27 14:23:14 +0200

    Remove 5 items per page option

--- src/component/DashboardComponent.js
+++ src/component/DashboardComponent.js
@@ -69,7 +69,6 @@ export default function DashboardComponent() {
                                 value={size}
                                 onChange={(e) => handlePageSizeChange(e)}
                             >
-                                <option value={5}>5</option>
                                 <option value={10}>10</option>
                                 <option value={25}>25</option>
                                 <option value={50}>50</option>
