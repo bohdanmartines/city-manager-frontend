53efdd54ed86ea478b408522903fe45b02e1ce2c
Bohdan Martines <bohdan.martines@gmail.com>
2024-10-07 15:42:41 +0200

    Header responsive design

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import {clearAuthTokens} from "../helper/session_state_helper";
-import {Link, useNavigate} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import {HOME, LOGIN} from "../helper/path";
+import {RiLogoutBoxRLine} from "react-icons/ri";
 
 export default function Header({logoSrc, pageTitle, loggedIn, setLoggedIn}) {
 
@@ -15,15 +16,20 @@ export default function Header({logoSrc, pageTitle, loggedIn, setLoggedIn}) {
 
     return (
         <header className="App-header">
-            <div className="row">
-                <div className="col-4"></div>
+            <div className="row justify-content-between justify-content-md-around">
                 <div className="col-4 text-center clickable" onClick={() => navigate(HOME)}>
-                    <img src={logoSrc} alt="logo" className="App-logo"/>
-                    <h1 className="App-title">{pageTitle}</h1>
+                    <div className="d-flex align-items-center">
+                        <img src={logoSrc} alt="logo" className="App-logo"/>
+                        <h1 className="App-title">{pageTitle}</h1>
+
+                    </div>
                 </div>
-                <div className="col-3"></div>
-                <div className="col-1 d-flex align-items-center">
-                    {loggedIn && <button className="btn btn-outline-info" onClick={logout}>Logout</button>}
+                <div className="col-2 d-flex align-items-center justify-content-end logout-sign" onClick={logout}>
+                    {loggedIn && <div className="d-flex align-items-center">
+                        <RiLogoutBoxRLine size={28}/>
+                        <div className="d-none d-md-block ms-2">Logout</div>
+                    </div>}
+
                 </div>
             </div>
         </header>
