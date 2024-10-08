c6bd4a8bdadeb687bb8e9fbe35c24aa1b170f466
Bohdan Martines <bohdan.martines@gmail.com>
2024-10-08 10:44:02 +0200

    Dashboard. Responsive pagination. v1

--- src/component/DashboardComponent.js
+++ src/component/DashboardComponent.js
@@ -61,47 +61,46 @@ export default function DashboardComponent() {
                         <h3 className="mb-2">Dashboard</h3>
                         <p className="lead">View existing tickets or create a new one <Link to={NEW_TICKET}>here</Link>.
                         </p>
-                        <div
-                            className="d-flex flex-column flex-md-row justify-content-between align-items-center mt-4 mb-3 overflow-auto">
-                            <Pagination className="m-0" size="sm">
-                                <Pagination.First onClick={() => setPage(0)} disabled={page === 0}/>
-                                <Pagination.Item onClick={() => handleJump(-10)}
-                                                 disabled={page < maxPages}>-10</Pagination.Item>
-                                <Pagination.Prev onClick={() => setPage(page - 1)} disabled={page === 0}/>
-                                {getRelevantPages().map((value) => (
-                                    <Pagination.Item
-                                        key={value}
-                                        active={value === page}
-                                        onClick={() => setPage(value)}
+                        <div className="container-fluid">
+                            <div className="row">
+                                <Pagination className="col mt-2 mb-0" size="sm">
+                                    <Pagination.First onClick={() => setPage(0)} disabled={page === 0}/>
+                                    <Pagination.Item onClick={() => handleJump(-10)}
+                                                     disabled={page < maxPages}>-10</Pagination.Item>
+                                    <Pagination.Prev onClick={() => setPage(page - 1)} disabled={page === 0}/>
+                                    {getRelevantPages().map((value) => (
+                                        <Pagination.Item
+                                            key={value}
+                                            active={value === page}
+                                            onClick={() => setPage(value)}
+                                        >
+                                            {value + 1}
+                                        </Pagination.Item>
+                                    ))}
+                                    <Pagination.Next onClick={() => setPage(page + 1)}
+                                                     disabled={page === totalPages - 1}/>
+                                    <Pagination.Item onClick={() => handleJump(10)}
+                                                     disabled={page > totalPages - (maxPages + 1)}>+10</Pagination.Item>
+                                    <Pagination.Last onClick={() => setPage(totalPages - 1)}
+                                                     disabled={page === totalPages - 1}/>
+                                </Pagination>
+                                <div className="col col-auto d-flex justify-content-end align-items-center mt-2 px-0">
+                                    <label htmlFor="ticketsPerPage" className="text-nowrap me-2">Show tickets:</label>
+                                    <select
+                                        id="ticketsPerPage"
+                                        className="form-select page-size-select"
+                                        value={size}
+                                        onChange={(e) => handlePageSizeChange(e)}
                                     >
-                                        {value + 1}
-                                    </Pagination.Item>
-                                ))}
-                                <Pagination.Next onClick={() => setPage(page + 1)} disabled={page === totalPages - 1}/>
-                                <Pagination.Item onClick={() => handleJump(10)}
-                                                 disabled={page > totalPages - (maxPages + 1)}>+10</Pagination.Item>
-                                <Pagination.Last onClick={() => setPage(totalPages - 1)}
-                                                 disabled={page === totalPages - 1}/>
-                            </Pagination>
-                            <div className="d-flex justify-content-end align-items-center">
-                                <label htmlFor="ticketsPerPage" className="text-nowrap me-3">Tickets per page:</label>
-                                <select
-                                    id="ticketsPerPage"
-                                    className="form-select"
-                                    value={size}
-                                    onChange={(e) => handlePageSizeChange(e)}
-                                >
-                                    <option value={10}>10</option>
-                                    <option value={25}>25</option>
-                                    <option value={50}>50</option>
-                                    <option value={100}>100</option>
-                                </select>
+                                        <option value={10}>10</option>
+                                        <option value={25}>25</option>
+                                        <option value={50}>50</option>
+                                        <option value={100}>100</option>
+                                    </select>
+                                </div>
                             </div>
                         </div>
-                        <div>
-
-                        </div>
-                        <div className="table-responsive">
+                        <div className="table-responsive mt-1">
                             <table className="table table-striped table-hover">
                                 <thead>
                                 <tr>
