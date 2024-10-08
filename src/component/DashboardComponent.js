8931151845a9628d8eeb7b04384f9d59e0607719
Bohdan Martines <bohdan.martines@gmail.com>
2024-10-08 10:19:23 +0200

    Dashboard. Add outer container

--- src/component/DashboardComponent.js
+++ src/component/DashboardComponent.js
@@ -48,78 +48,84 @@ export default function DashboardComponent() {
         if (totalPages <= maxPages) {
             return [...Array(totalPages)].map((value, index) => index);
         }
-        const startPage = Math.floor(page/maxPages) * maxPages;
+        const startPage = Math.floor(page / maxPages) * maxPages;
         const endPage = Math.min(totalPages - 1, startPage + maxPages - 1);
         return Array.from({length: endPage - startPage + 1}, (_, i) => startPage + i);
     }
 
     return (
-        <div className="row justify-content-center">
-            <div className="col-12 col-md-7">
-                <div className="mt-4">
-                    <h3 className="mb-2">Dashboard</h3>
-                    <p className="lead">View existing tickets or create a new one <Link to={NEW_TICKET}>here</Link>.</p>
-                    <div className="d-flex flex-column flex-md-row justify-content-between align-items-center mt-4 mb-3 overflow-auto">
-                        <Pagination className="m-0" size="sm">
-                            <Pagination.First onClick={() => setPage(0)} disabled={page === 0}/>
-                            <Pagination.Item onClick={() => handleJump(-10)} disabled={page < maxPages}>-10</Pagination.Item>
-                            <Pagination.Prev onClick={() => setPage(page - 1)} disabled={page === 0}/>
-                            {getRelevantPages().map((value) => (
-                                <Pagination.Item
-                                    key={value}
-                                    active={value === page}
-                                    onClick={() => setPage(value)}
+        <div className="container-fluid">
+            <div className="row justify-content-center">
+                <div className="col-12 col-md-7">
+                    <div className="mt-4">
+                        <h3 className="mb-2">Dashboard</h3>
+                        <p className="lead">View existing tickets or create a new one <Link to={NEW_TICKET}>here</Link>.
+                        </p>
+                        <div
+                            className="d-flex flex-column flex-md-row justify-content-between align-items-center mt-4 mb-3 overflow-auto">
+                            <Pagination className="m-0" size="sm">
+                                <Pagination.First onClick={() => setPage(0)} disabled={page === 0}/>
+                                <Pagination.Item onClick={() => handleJump(-10)}
+                                                 disabled={page < maxPages}>-10</Pagination.Item>
+                                <Pagination.Prev onClick={() => setPage(page - 1)} disabled={page === 0}/>
+                                {getRelevantPages().map((value) => (
+                                    <Pagination.Item
+                                        key={value}
+                                        active={value === page}
+                                        onClick={() => setPage(value)}
+                                    >
+                                        {value + 1}
+                                    </Pagination.Item>
+                                ))}
+                                <Pagination.Next onClick={() => setPage(page + 1)} disabled={page === totalPages - 1}/>
+                                <Pagination.Item onClick={() => handleJump(10)}
+                                                 disabled={page > totalPages - (maxPages + 1)}>+10</Pagination.Item>
+                                <Pagination.Last onClick={() => setPage(totalPages - 1)}
+                                                 disabled={page === totalPages - 1}/>
+                            </Pagination>
+                            <div className="d-flex justify-content-end align-items-center">
+                                <label htmlFor="ticketsPerPage" className="text-nowrap me-3">Tickets per page:</label>
+                                <select
+                                    id="ticketsPerPage"
+                                    className="form-select"
+                                    value={size}
+                                    onChange={(e) => handlePageSizeChange(e)}
                                 >
-                                    {value + 1}
-                                </Pagination.Item>
-                            ))}
-                            <Pagination.Next onClick={() => setPage(page + 1)} disabled={page === totalPages - 1}/>
-                            <Pagination.Item onClick={() => handleJump(10)} disabled={page > totalPages - (maxPages + 1)}>+10</Pagination.Item>
-                            <Pagination.Last onClick={() => setPage(totalPages - 1)}
-                                             disabled={page === totalPages - 1}/>
-                        </Pagination>
-                        <div className="d-flex justify-content-end align-items-center">
-                            <label htmlFor="ticketsPerPage" className="text-nowrap me-3">Tickets per page:</label>
-                            <select
-                                id="ticketsPerPage"
-                                className="form-select"
-                                value={size}
-                                onChange={(e) => handlePageSizeChange(e)}
-                            >
-                                <option value={10}>10</option>
-                                <option value={25}>25</option>
-                                <option value={50}>50</option>
-                                <option value={100}>100</option>
-                            </select>
+                                    <option value={10}>10</option>
+                                    <option value={25}>25</option>
+                                    <option value={50}>50</option>
+                                    <option value={100}>100</option>
+                                </select>
+                            </div>
                         </div>
-                    </div>
-                    <div>
+                        <div>
 
-                    </div>
-                    <div className="table-responsive">
-                        <table className="table table-striped table-hover">
-                            <thead>
-                            <tr>
-                                <th scope="col">Ticket ID</th>
-                                <th scope="col">Title</th>
-                                <th scope="col">Description</th>
-                                <th scope="col">Status</th>
-                                <th scope="col">Created At</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            {tickets.map((ticket) => (
-                                <tr key={ticket.id} className="clickable"
-                                    onClick={() => navigate(`${TICKET_DETAILS_BASE_PATH}/${ticket.id}`)}>
-                                    <th scope="row">{ticket.id}</th>
-                                    <td className="dashboard-text">{ticket.title}</td>
-                                    <td className="dashboard-text">{ticket.description}</td>
-                                    <td><TicketStatusBadge status={ticket.status}/></td>
-                                    <td>{toLocaleDateString(ticket.createdAt)}</td>
+                        </div>
+                        <div className="table-responsive">
+                            <table className="table table-striped table-hover">
+                                <thead>
+                                <tr>
+                                    <th scope="col">Ticket ID</th>
+                                    <th scope="col">Title</th>
+                                    <th scope="col">Description</th>
+                                    <th scope="col">Status</th>
+                                    <th scope="col">Created At</th>
                                 </tr>
-                            ))}
-                            </tbody>
-                        </table>
+                                </thead>
+                                <tbody>
+                                {tickets.map((ticket) => (
+                                    <tr key={ticket.id} className="clickable"
+                                        onClick={() => navigate(`${TICKET_DETAILS_BASE_PATH}/${ticket.id}`)}>
+                                        <th scope="row">{ticket.id}</th>
+                                        <td className="dashboard-text">{ticket.title}</td>
+                                        <td className="dashboard-text">{ticket.description}</td>
+                                        <td><TicketStatusBadge status={ticket.status}/></td>
+                                        <td>{toLocaleDateString(ticket.createdAt)}</td>
+                                    </tr>
+                                ))}
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
